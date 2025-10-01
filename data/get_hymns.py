80ef853ea3b2c4bad956a8cc1199410d426e6523
Jake Surgeon <jakobsurge@gmail.com>
2025-09-30 22:17:49 -0400

    Update URL for hymn data to sort by totalInstances, be limited to 100, and construct URL with urlencode for better legibility

--- data/get_hymns.py
+++ data/get_hymns.py
@@ -1,10 +1,23 @@
 import pandas as pd
+from urllib.parse import urlencode
 
-# import csv from Hymnary and retrieve first 100 hymn titles
-# the url is using advanced search to filter for public domain, English langauge, text-only files, and Church of God denomination
-url = "https://hymnary.org/texts?qu=textLanguages%3Aenglish%20denominations%3Achurch%20of%20god%20media%3Atext%20textClassification%3Atextispublicdomain%20tuneClassification%3Atuneispublicdomain%20in%3Atexts&export=csv&limit=384"
+# import csv from Hymnary
+# the url uses advanced search to filter for multiple things outlined in the params
+base_url = "https://hymnary.org/search"
+
+params = {
+    "qu": "textLanguages:english denominations:church of god media:text "
+    "textClassification:textispublicdomain tuneClassification:tuneispublicdomain in:texts",
+    "sort": "totalInstances",
+    "export": "csv",
+    "limit": 100
+}
+
+query_string = urlencode(params)
+url = f"{base_url}?{query_string}"
+
 hymns = pd.read_csv(url)
-hymn_titles = hymns['displayTitle'].head(100)
+hymn_titles = hymns['displayTitle']
 
 # export as .json
 hymn_titles.to_json("hymns.json", compression='infer', index=False)
