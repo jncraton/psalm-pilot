0ff321da21173f92adf16fe7d1f3d37469f2e0ff
MacKellar Sample <159157792+msample2@users.noreply.github.com>
2025-10-03 13:43:39 -0400

    implement service worker

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('psalm-pilot-cache').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/style.css',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
