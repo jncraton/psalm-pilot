19b9bdc233230e9f60371b5b3e3f78c5f6f522df
Asher McKenna <mr.ashermckenna@gmail.com>
2025-10-03 14:25:59 -0400

    Run prettier

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('psalm-pilot-cache').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/style.css',
-    ])),
-  );
-});
+    caches
+      .open('psalm-pilot-cache')
+      .then(cache => cache.addAll(['/', '/index.html', '/style.css'])),
+  )
+})
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
+self.addEventListener('fetch', e => {
+  console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+    caches.match(e.request).then(response => response || fetch(e.request)),
+  )
+})
