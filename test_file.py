0673d1b19573b4beafe2a1632f8c5c6a5c413ee9
jillcrawford <jrcrawford@anderson.edu>
2025-10-14 13:52:50 -0400

    Make Playwright open html file to run tests

--- test_file.py
+++ test_file.py
@@ -1,2 +1,10 @@
 import re
+import os
+import pytest
 from playwright.sync_api import Page, expect
+
+def test_html_file(page: Page):
+    path = os.path.abspath('index.html')
+    page.goto(f"file://{path}")
+
+    assert page.title() == "Palm Pilot"
