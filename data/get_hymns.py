49f9904ead55853084ee9c0feb4272e39013f38e
Asher McKenna <mr.ashermckenna@gmail.com>
2025-10-14 12:44:00 -0400

    Add logic for NaN values

--- data/get_hymns.py
+++ data/get_hymns.py
@@ -20,14 +20,18 @@ url = f"{base_url}?{query_string}"
 hymns = pd.read_csv(url)
 hymn_titles = hymns[['displayTitle', 'authors']]
 
-output = {
-    str(field): {
-        "title": row['displayTitle'],
-        "author": row['authors']
-    }
-    for field, row in hymn_titles.iterrows()
-}
+output = {}
 
+# logic for if an author is not recorded so it doesn't show up as an error
+for field, row in hymn_titles.iterrows():
+    author = row['authors']
+    if pd.isna(author):
+        author = "Author not recorded"
+    output[str(field)] = {
+        "title": row['displayTitle'],
+        "author": author
+    }
+        
 # export as .json
 with open("hymns.json", "w") as f:
     json.dump(output, f, indent=2)
