971292d06bea2361100ac7758483bf1b849c6324
MacKellar Sample <159157792+msample2@users.noreply.github.com>
2025-10-15 13:51:48 -0400

    implement old cache deletion capability

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,31 @@
+const CACHE_NAME = 'psalm-pilot-cache-v1'
+
 self.addEventListener('install', e => {
   e.waitUntil(
     caches
-      .open('psalm-pilot-cache')
-      .then(cache => cache.addAll(['/', '/index.html', '/styles.css']))
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(['/', '/index.html', '/styles.css'])),
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache)
+            return caches.delete(cache)
+          }
+        }),
+      )
+    }),
   )
 })
 
 self.addEventListener('fetch', e => {
   console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request)),
   )
 })
