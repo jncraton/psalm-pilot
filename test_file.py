c70b10202ca147b6ed454ff91260407282922112
Caroline Derolf <cnderolf325@gmail.com>
2025-10-15 13:33:19 -0400

    make test- table row counter

--- test_file.py
+++ test_file.py
@@ -1,6 +1,4 @@
-import re
 import os
-import pytest
 from playwright.sync_api import Page, expect
 
 def test_html_file(page: Page):
@@ -8,3 +6,12 @@ def test_html_file(page: Page):
     page.goto(f"file://{path}")
 
     expect(page).to_have_title("Psalm Pilot")
+
+def test_table_not_empty(page: Page):
+    path = os.path.abspath('index.html')
+    page.goto(f"file://{path}")
+
+    row_locator = page.locator("table tbody tr")
+    # row_count=row_locator.count()
+    # print({row_count})
+    expect(row_locator).not_to_have_count(0)
