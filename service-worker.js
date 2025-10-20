81e9e979420399d05bb686941eb9b6bc13efd15a
MacKellar Sample <159157792+msample2@users.noreply.github.com>
2025-10-20 13:45:58 -0400

    run lint

--- service-worker.js
+++ service-worker.js
@@ -5,28 +5,31 @@ self.addEventListener('install', e => {
     caches
       .open(CACHE_NAME)
       .then(cache => cache.addAll(['/', '/index.html', '/styles.css']))
-      .then(() => self.skipWaiting())
+      .then(() => self.skipWaiting()),
   )
 })
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache)
-            return caches.delete(cache)
-          }
-        })
-      )
-    }).then(() => self.clients.claim()) // ensure active control of pages
+    caches
+      .keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== CACHE_NAME) {
+              console.log('Deleting old cache:', cache)
+              return caches.delete(cache)
+            }
+          }),
+        )
+      })
+      .then(() => self.clients.claim()),
   )
 })
 
 self.addEventListener('fetch', e => {
   console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request)),
   )
 })
