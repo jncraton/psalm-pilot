2dc6b93938496274d028099083e2732f142ac808
Asher McKenna <mr.ashermckenna@gmail.com>
2025-10-21 13:49:47 -0400

    Run linter

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', e => {
     caches
       .open(CACHE_NAME)
       .then(cache => cache.addAll(['index.html', 'styles.css']))
-      .then(() => self.skipWaiting())
+      .then(() => self.skipWaiting()),
   )
 })
 
@@ -20,16 +20,16 @@ self.addEventListener('activate', e => {
               console.log('Deleting old cache:', cache)
               return caches.delete(cache)
             }
-          })
+          }),
         )
       })
-      .then(() => self.clients.claim())
+      .then(() => self.clients.claim()),
   )
 })
 
 self.addEventListener('fetch', e => {
   console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request)),
   )
 })
