0d6311f435cbb6a9f6230e93ce460a1f71341795
Asher McKenna <mr.ashermckenna@gmail.com>
2025-10-22 07:27:47 -0500

    Lint it

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ self.addEventListener('install', e => {
           new URL('styles.css', base).toString(),
         ])
       })
-      .then(() => self.skipWaiting())
+      .then(() => self.skipWaiting()),
   )
 })
 
@@ -26,16 +26,16 @@ self.addEventListener('activate', e => {
               console.log('Deleting old cache:', cache)
               return caches.delete(cache)
             }
-          })
+          }),
         )
       })
-      .then(() => self.clients.claim())
+      .then(() => self.clients.claim()),
   )
 })
 
 self.addEventListener('fetch', e => {
   console.log(e.request.url)
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request)),
   )
 })
