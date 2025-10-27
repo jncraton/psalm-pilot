2e0613a1ffc6b46e2d00c2b5ec656040292e3027
nickpavey <nicholaspavey03@gmail.com>
2025-10-27 12:59:24 -0400

    Shortened code. Looks a lot cleaner

--- data/get_hymns.py
+++ data/get_hymns.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from urllib.parse import urlencode
-import json
 import numpy as np
 
 # import csv from Hymnary
 # the url uses advanced search to filter for multiple things outlined in the params
 base_url = "https://hymnary.org/search"
+# General database for identifying the years and to match authNums
+texts_url = "https://hymnary.org/files/hymnary/other/texts.csv"
 
 params = {
     "qu": {
@@ -26,36 +27,24 @@ query_string = urlencode(params)
 url = f"{base_url}?{query_string}"
 
 hymns = pd.read_csv(url)
-
-hymns = hymns[['displayTitle', 'authors', 'textAuthNumber']]
-
-hymns = hymns.rename(columns={'displayTitle': 'title'})
-
-hymns.replace({np.nan: None}, inplace=True)
-
-# Retrieve texts
-texts_url = "https://hymnary.org/files/hymnary/other/texts.csv"
 texts = pd.read_csv(texts_url)
 
-merged = pd.merge(hymns, texts[['textAuthNumber', 'yearsWrote']], on='textAuthNumber', how='left')
+hymns.replace({np.nan: None}, inplace=True)
+hymns = hymns[['displayTitle', 'authors', 'textAuthNumber']]
+hymns = hymns.rename(columns={
+    'displayTitle': 'title',
+    'textAuthNumber':'authNum'
+})
+texts = texts[['displayTitle', 'authors', 'textAuthNumber', 'yearsWrote']]
+texts = texts.rename(columns={
+    'displayTitle': 'title',
+    'textAuthNumber':'authNum',
+    'yearsWrote':'publicationDate'
+})
 
-# File write and appending to JSON file
-results = []
-for _, row in merged.iterrows():
-    title = str(row.get("title", "")).strip()
-    authors = str(row.get("authors", "")).strip()
-    pub_date = (
-        str(row.get("yearsWrote", "")).strip()
-        if pd.notna(row.get("yearsWrote"))
-        else None
-    )
+merged_txts = pd.merge(hymns, texts[['authNum', 'publicationDate']], on='authNum', how='left')
 
-    results.append({
-        "title": title,
-        "authors": authors,
-        "publicationDate": pub_date
-    })
+merged_txts = merged_txts.drop(columns=['authNum'])
 
-
-with open("data/hymns.json", "w", encoding="utf-8") as f:
-    json.dump(results, f, indent=2)
+# export to json
+merged_txts.to_json("data/hymns.json", orient="records", indent=2)
