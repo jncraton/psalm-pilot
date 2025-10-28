548b4bdc1939a81f844a6c500d77dfe868f113fb
Jakob Surgeon <52480958+JakeSurge@users.noreply.github.com>
2025-10-28 13:58:20 -0400

    Create test for song authors
    * Update to fixture for grabbing the hymn source data
    
    * Add test case for viewing song authors
    
    * Update method of grabbing column cells to work via column name instead of static index
    
    * Update test_file.py
    
    Co-authored-by: Jon Craton <jncraton@gmail.com>
    
    ---------
    
    Co-authored-by: Jon Craton <jncraton@gmail.com>

--- test_file.py
+++ test_file.py
@@ -1,7 +1,7 @@
 import os
 import json
 import pytest
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, Locator, expect
 
 @pytest.fixture
 def main_page(page: Page) -> Page:
@@ -9,6 +9,38 @@ def main_page(page: Page) -> Page:
     page.goto(f"file://{os.path.abspath('index.html')}")
 
     return page
+
+
+@pytest.fixture
+def hymn_data() -> list:
+    """Grabs the hymns source data for verification comparison."""
+    with open('data/hymns.json', 'r') as file:
+        data = json.load(file)
+    
+    return data
+
+
+def get_column_index(page: Page, column_name: str) -> int:
+    """Finds the index of a column by the name of it"""
+    # Grab header cells
+    header_cells = page.locator("th")
+
+    # Find the the matching column name
+    for i in range(header_cells.count()):
+        if column_name in header_cells.nth(i).inner_html():
+            return i + 1
+
+    # If not found return error
+    raise ValueError(f"Column name {column_name} not found")
+
+
+def get_column_cells(page: Page, column_name: str) -> Locator:
+    """Grabs the cells of a column by the name of it"""
+    # Find the column index
+    column_index = get_column_index(page, column_name)
+
+    # Return the locator of that index
+    return page.locator(f"td:nth-child({column_index})")
 
 
 def test_html_file(main_page: Page):
@@ -20,15 +52,31 @@ def test_table_not_empty(main_page: Page):
     expect(row_locator).not_to_have_count(0)
 
 
-def test_browse_hymns(main_page: Page):
-    # Grab the hymn source data
-    with open('data/hymns.json', 'r') as file:
-        hymns = json.load(file)
-
-    hymn_titles = [hymn['title'] for hymn in hymns]
+def test_song_titles(main_page: Page, hymn_data: list):
+    # Grab hymn title source data
+    hymn_titles = [hymn['title'] for hymn in hymn_data]
 
     # Grab the hymn title column cells
-    hymn_title_cells = main_page.locator("td:nth-child(1)")
+    hymn_title_cells = get_column_cells(main_page, 'Title')
 
     # Verify the data matches
     expect(hymn_title_cells).to_have_text(hymn_titles)
+
+
+def test_song_authors(main_page: Page, hymn_data: list):
+    # Grab hymn authors source data
+    hymn_authors = [hymn['authors'] for hymn in hymn_data]
+
+    # Grab the hymn authors column cells
+    hymn_author_cells = get_column_cells(main_page, 'Author')
+
+    # Verify the cells and data are of same length
+    expect(hymn_author_cells).to_have_count(len(hymn_authors))
+
+    # Verify the data matches
+    for hymn_author_cell, hymn_author in zip(hymn_author_cells.all(), hymn_authors):
+        
+        if hymn_author is not None:
+            expect(hymn_author_cell).to_have_text(hymn_author)
+        else:
+            expect(hymn_author_cell).to_be_empty()
