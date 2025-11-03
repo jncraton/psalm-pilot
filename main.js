d839349658498576fac78ec475bdf123ae81e77c
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-11-03 13:52:42 -0500

    We change the css and get rid of the animation. And chnage the behavior of the search bar

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ function search_title() {
     
     let input = document.getElementById('search').value
     input = input.toLowerCase();
-    let title = document.getElementsByClassName('hymn-title');
+    console.log(input);
+
+    let title = document.querySelectorAll('tr');
 
     for (i = 0; i < title.length; i++) {
         const whole_row = title[i].closest('tr');
         if (title[i].textContent.toLowerCase().includes(input)) {
-            whole_row.style.display = "";
+            whole_row.style.display = ""
         }
         else {
             whole_row.style.display = "none";
         }
     }
 }
+
