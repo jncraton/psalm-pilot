46040e0a9c87b056937e60dcdcdc0860e5b32e50
Caroline Derolf <cnderolf325@gmail.com>
2025-11-05 13:27:25 -0500

    adjust comments in for loop

--- test_file.py
+++ test_file.py
@@ -100,16 +100,16 @@ def test_song_years(main_page: Page, hymn_data: list):
 
 def test_song_lyrics(main_page:Page, hymn_data: list):
     for hymn in hymn_data:
-    # Grab the title source data
+        # Grab the title source data
         hymn_title_id = hymn['titleId']
 
-    # Grab hymn lyrics source data
+        # Grab hymn lyrics source data
         hymn_lyrics = hymn['text']
 
-    # Go to next page 
+        # Go to next page 
         main_page.goto(Path(f"hymns/{hymn_title_id}.html").resolve().as_uri())
         
-    # Confirm the hymn lyrics on the new page
+        # Confirm the hymn lyrics on the new page
         expect(main_page.locator("p")).to_contain_text(hymn_lyrics)
 
 
