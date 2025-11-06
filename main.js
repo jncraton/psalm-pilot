88cf70aa1d36d235c4a8e4d027d5cf72cc58bed5
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-11-06 12:10:57 -0500

    Lint

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 function search_title() {
-    
-    let input = document.getElementById('search').value
-    input = input.toLowerCase();
-    console.log(input);
+  let input = document.getElementById('search').value
+  input = input.toLowerCase()
+  console.log(input)
 
-    let title = document.querySelectorAll('tr');
+  let title = document.querySelectorAll('tr')
 
-    for (i = 0; i < title.length; i++) {
-        const whole_row = title[i].closest('tr');
-        if (title[i].textContent.toLowerCase().includes(input)) {
-            whole_row.style.display = ""
-        }
-        else {
-            whole_row.style.display = "none";
-        }
+  for (i = 0; i < title.length; i++) {
+    const whole_row = title[i].closest('tr')
+    if (title[i].textContent.toLowerCase().includes(input)) {
+      whole_row.style.display = ''
+    } else {
+      whole_row.style.display = 'none'
     }
+  }
 }
-
