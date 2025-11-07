82d5eb55bf848f12eb5f9b1d071126ef5f7d51ab
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-11-07 15:14:21 -0500

    Search bar in one place and still the table have a title and other paramenters while searching

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 function search_title() {
   let input = document.getElementById('search').value
-  input = input.toLowerCase()
-  console.log(input)
 
-  let title = document.querySelectorAll('tr')
+  input = input.toLowerCase()
+
+  let title = document.querySelectorAll('tbody tr')
+  console.log(title)
 
   for (i = 0; i < title.length; i++) {
     const whole_row = title[i].closest('tr')
+    // var evenRows = document.querySelectorAll('tbody tr:nth-child(even)');
+
     if (title[i].textContent.toLowerCase().includes(input)) {
+      // evenRows.forEach(function(row) {
+      //   row.style.backgroundColor = "#f1f1f1";
+      // });
+      title.forEach(titleElement => {
+        titleElement.style.backgroundColor = 'transparent'
+      })
       whole_row.style.display = ''
     } else {
       whole_row.style.display = 'none'
