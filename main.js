95681701c7c028e7183fcf62ad77eaf4d24e6751
Sarvar Muzaffarov <smuzaffarov@anderson.edu>
2025-11-09 11:29:46 -0500

    Search bar COMPLITE, we did every changes

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 function search_title() {
   let input = document.getElementById('search').value
-
   input = input.toLowerCase()
-
   let title = document.querySelectorAll('tbody tr')
-  console.log(title)
+
 
   for (i = 0; i < title.length; i++) {
     const whole_row = title[i].closest('tr')
-    // var evenRows = document.querySelectorAll('tbody tr:nth-child(even)');
-    
 
     if (title[i].textContent.toLowerCase().includes(input)) {
-      // evenRows.forEach(function(row) {
-      //   row.style.backgroundColor = "#f1f1f1";
-      // });
-      title.forEach(titleElement => {
-        titleElement.style.backgroundColor = "transparent";
-      });
       whole_row.style.display = ''
     } else {
       whole_row.style.display = 'none'
     }
+
+
+    const visibleRows = [];
+    for (let i = 0; i < title.length; i++) {
+      if (title[i].style.display !== 'none') {
+      visibleRows.push(title[i]);
+      }
+    }
+    for (let i = 0; i < visibleRows.length; i++) {
+      if (i % 2 === 0) {
+        visibleRows[i].style.backgroundColor = '#f1f1f1';
+      } else {
+        visibleRows[i].style.backgroundColor = 'transparent';
+      }
+    }
   }
-}
+};
