159fb19fd2f79689bea7758f3abc1d08578ce45f
nickpavey <116367594+nickpavey@users.noreply.github.com>
2025-11-10 13:34:42 -0500

    Added HTML Parser and scraper (#89)
    * Added HTML Parser and scraper
    
    * Update get_hymns.py
    
    * Update get_hymns.py
    
    * Update get_hymns.py
    
    * df
    
    f

--- data/get_hymns.py
+++ data/get_hymns.py
@@ -1,12 +1,13 @@
-import pandas as pd
 from urllib.parse import urlencode
+from urllib.request import urlopen
+from html.parser import HTMLParser
+import pandas as pd
 import numpy as np
 import requests
 
 # import csv from Hymnary
 # the url uses advanced search to filter for multiple things outlined in the params
 base_url = "https://hymnary.org/search"
-# General database for identifying the years and to match authNums
 texts_url = "https://hymnary.org/files/hymnary/other/texts.csv"
 
 params = {
@@ -24,15 +25,53 @@ params = {
 }
 
 params["qu"] = " ".join([f"{k}:{v}" for k, v in params["qu"].items()])
-query_string = urlencode(params)
-url = f"{base_url}?{query_string}"
+url = f"{base_url}?{urlencode(params)}"
 
 hymns = pd.read_csv(url)
-
 texts = pd.read_csv(texts_url)
 hymns = hymns.merge(texts[['textAuthNumber', 'yearsWrote']], on='textAuthNumber', how='left')
 
 hymns['popularity'] = (100 * hymns['totalInstances'] / hymns['totalInstances'].max()).astype(int)
+
+class ScriptureHTMLParser(HTMLParser):
+    #Set up parser
+    def __init__(self):
+        super().__init__()
+        self.in_ref = False
+        self.references = []
+
+    #Inside scripture div
+    def handle_starttag(self, tag, attrs):
+        if tag == "div" and ("class", "scripture_reference") in attrs:
+            self.in_ref = True
+
+    #Outside scripture div
+    def handle_endtag(self, tag):
+        if tag == "div" and self.in_ref:
+            self.in_ref = False
+
+    #Append references to scripture
+    def handle_data(self, data):
+        if self.in_ref:
+            ref = data.strip()
+            if ref:
+                self.references.append(ref)
+
+def scrape_scripture_references(text_auth_number):
+    url = f"https://hymnary.org/text/{text_auth_number}#text-scripture"
+    try:
+        with urlopen(url) as response:
+            html = response.read().decode("utf-8")
+        parser = ScriptureHTMLParser()
+        parser.feed(html)
+        return parser.references if parser.references else None
+    except Exception:
+        print(f"Scrape failed for {text_auth_number}")
+        return None
+
+hymns['scriptureReferences'] = hymns['textAuthNumber'].apply(scrape_scripture_references)
+hymns.replace({np.nan: None}, inplace=True)
+hymns = hymns[['textAuthNumber','displayTitle', 'authors', 'popularity', 'yearsWrote', 'scriptureReferences']]
 
 def get_text(textAuthNumber):
     print(f"Downloading text for {textAuthNumber}...")
@@ -43,12 +82,13 @@ def get_text(textAuthNumber):
 hymns['text'] = hymns['textAuthNumber'].apply(get_text)
 
 hymns.replace({np.nan: None}, inplace=True)
-hymns = hymns[['textAuthNumber', 'displayTitle', 'authors', 'text', 'popularity', 'yearsWrote']]
+hymns = hymns[['textAuthNumber', 'displayTitle', 'authors', 'text', 'popularity', 'yearsWrote','scriptureReferences']]
 
 hymns = hymns.rename(columns={
     'textAuthNumber': 'titleId',
     'displayTitle': 'title',
-    'yearsWrote': 'publicationYear'
+    'yearsWrote': 'publicationYear',
+    'scriptureReferences': 'scriptureRefs',
 })
 
 # export to json
