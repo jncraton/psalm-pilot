916fbf06f8432df09cd4796bee61019176a6b6b5
Jake Surgeon <jakobsurge@gmail.com>
2025-11-14 14:36:22 -0500

    Update render script and tests for www path

--- tests/test_hymn_pages.py
+++ tests/test_hymn_pages.py
@@ -12,14 +12,14 @@ def test_song_lyrics(page: Page, hymn_data: list):
         hymn_lyrics = hymn['text']
 
         # Go to next page 
-        page.goto(Path(f"hymns/{hymn_title_id}.html").resolve().as_uri())
+        page.goto(Path(f"www/hymns/{hymn_title_id}.html").resolve().as_uri())
         
         # Confirm the hymn lyrics on the new page
         expect(page.locator("blockquote")).to_contain_text(hymn_lyrics)
 
 def test_hymn_link(page: Page, hymn_data: list):
     for hymn in hymn_data[:2] + hymn_data[-2:]:
-        page.goto(Path(f"hymns/{hymn['titleId']}.html").resolve().as_uri())
+        page.goto(Path(f"www/hymns/{hymn['titleId']}.html").resolve().as_uri())
 
         # Confirm 200 status on link click
         with page.expect_response(re.compile(r"hymnary.org")) as response_info:
