9f222c8b51d570d6207ad718a179c5f14873827c
Jake Surgeon <jakobsurge@gmail.com>
2025-11-16 18:06:46 -0500

    Update path of writing for hymns_list.json

--- render.py
+++ render.py
@@ -47,7 +47,7 @@ hymns = sorted(
     str(p.as_posix())
     for p in Path("hymns").rglob("*")
     if p.is_file())
-Path("hymns_list.json").write_text(json.dumps(hymns, indent=2), encoding="utf-8")
+Path("www/hymns_list.json").write_text(json.dumps(hymns, indent=2), encoding="utf-8")
 
 sw_template = env.get_template('service-worker.jinja')
     
