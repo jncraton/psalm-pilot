1484e8620180c146fa94638b2520e72b7ff2d8d5
jillcrawford <jrcrawford@anderson.edu>
2025-11-21 13:49:38 -0500

    Move chat function from base.jinja to main.js

--- main.js
+++ main.js
@@ -4,3 +4,42 @@ function search_title() {
     r.style.display = r.textContent.toLowerCase().includes(input) ? '' : 'none'
   })
 }
+
+async function chat(message, model = 'gemini-flash-lite-latest', tries = 3) {
+  if (tries < 1) {
+    throw new Error('Chat retries exceeded')
+  }
+
+  localStorage.geminiKey = localStorage.geminiKey || prompt('Enter Gemini key')
+
+  const res = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-goog-api-key': localStorage.geminiKey,
+      },
+      body: `{contents:[{parts:[{text:${JSON.stringify(message)}}]}]}`,
+    },
+  )
+
+  try {
+    if (res.status != 200) {
+      console.error(await res.json())
+      throw new Error(`Gemini API: ${res.status} ${res.statusText}`)
+    }
+    return (await res.json()).candidates[0].content.parts[0].text
+  } catch (error) {
+    console.error(error)
+    console.warn('Waiting 3 seconds before Gemini retry')
+    await new Promise(r => setTimeout(r, 3000))
+    return await chat(message, model, tries - 1)
+  }
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('{{ root_dir }}service-worker.js')
+  })
+}
